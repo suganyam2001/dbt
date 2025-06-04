fbfd00b2b47cc1d4d446a29814c760dd4c117a77
Suganya Muruganantham <suganya.m2001@gmail.com>
2025-06-04 17:16:53 +0000

    prep_housing_data .sql file changed with more columns

--- models/TEAM_4_DB/PREP/prep_housing_data.sql
+++ models/TEAM_4_DB/PREP/prep_housing_data.sql
@@ -1,10 +1,16 @@
 SELECT
-  ID,
-  STATE,
-  PRICE,
-  TYPE,
-  BEDS,
-  BATHS,
-  SQFEET,
-  PARKING_OPTIONS
+  id,
+  price,
+  type,
+  sqfeet,
+  beds,
+  baths,
+  cats_allowed,
+  dogs_allowed,
+  smoking_allowed,
+  wheelchair_access,
+  electric_vehicle_charge,
+  comes_furnished,
+  parking_options,
+  state
 FROM {{ source('raw', 'HOUSING_CSV_RAW') }}
