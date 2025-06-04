08603e8eaac2fc2f77c71f7d8ae926252494c46d
Suganya Muruganantham <suganya.m2001@gmail.com>
2025-06-04 17:13:08 +0000

    Column names modified in the Json file prep_qol_data

--- models/TEAM_4_DB/PREP/prep_qol_data.sql
+++ models/TEAM_4_DB/PREP/prep_qol_data.sql
@@ -1,9 +1,9 @@
 SELECT
-  JSON_DATA:"state"::STRING AS STATE,
-  JSON_DATA:"QualityOfLifeTotalScore"::NUMBER AS QUALITYOFLIFETOTALSCORE,
-  JSON_DATA:"QualityOfLifeQualityOfLife"::NUMBER AS QUALITYOFLIFEQUALITYOFLIFE,
-  JSON_DATA:"QualityOfLifeAffordability"::NUMBER AS QUALITYOFLIFEAFFORDABILITY,
-  JSON_DATA:"QualityOfLifeEconomy"::NUMBER AS QUALITYOFLIFEECONOMY,
-  JSON_DATA:"QualityOfLifeEducationAndHealth"::NUMBER AS QUALITYOFLIFEEDUCATIONANDHEALTH,
-  JSON_DATA:"QualityOfLifeSafety"::NUMBER AS QUALITYOFLIFESAFETY
+  JSON_DATA:"state"::STRING AS state,
+  JSON_DATA:"QualityOfLifeTotalScore"::NUMBER AS Qol_TotalScore,
+  JSON_DATA:"QualityOfLifeQualityOfLife"::NUMBER AS Qol_Life,
+  JSON_DATA:"QualityOfLifeAffordability"::NUMBER AS Qol_Affordability,
+  JSON_DATA:"QualityOfLifeEconomy"::NUMBER AS Qol_Economy,
+  JSON_DATA:"QualityOfLifeEducationAndHealth"::NUMBER AS Qol_Education_And_Health,
+  JSON_DATA:"QualityOfLifeSafety"::NUMBER AS Qol_Safety
 FROM {{ source('raw', 'QOL_JSON_RAW') }}
