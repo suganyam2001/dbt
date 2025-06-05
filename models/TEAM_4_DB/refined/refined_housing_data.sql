76ace32d2c00acae5c8957b03fb3d8203bd2c267
Suganya Muruganantham <suganya.m2001@gmail.com>
2025-06-05 12:23:00 +0000

    refined models updated to refer directly the snowflake to reflect the cleaned data directly.

--- models/TEAM_4_DB/refined/refined_housing_data.sql
+++ models/TEAM_4_DB/refined/refined_housing_data.sql
@@ -1,16 +1,2 @@
-SELECT
-  id,
-  price,
-  type,
-  sqfeet,
-  beds,
-  baths,
-  cats_allowed,
-  dogs_allowed,
-  smoking_allowed,
-  wheelchair_access,
-  electric_vehicle_charge,
-  comes_furnished,
-  parking_options,
-  state AS state
-FROM {{ ref('prep_housing_data') }}
+SELECT *
+FROM TEAM_4_DB.REFINED.HOUSING_CSV_REFINED
--- models/TEAM_4_DB/refined/refined_qol_data.sql
+++ models/TEAM_4_DB/refined/refined_qol_data.sql
@@ -1,9 +1,2 @@
-SELECT
-  state AS state,
-  Qol_TotalScore,
-  Qol_Life,
-  Qol_Affordability,
-  Qol_Economy,
-  Qol_Education_And_Health,
-  Qol_Safety
-FROM {{ ref('prep_qol_data') }}
+SELECT *
+FROM TEAM_4_DB.REFINED.QOL_JSON_REFINED
