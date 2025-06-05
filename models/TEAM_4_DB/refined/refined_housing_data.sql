a8854f4fe4e452fb1ebaa2c04ccacd77fc07ebe6
Suganya Muruganantham <suganya.m2001@gmail.com>
2025-06-05 13:11:37 +0000

    Refined models are not from snowflake but referring the prep and cleaning is updated in dbt

--- models/TEAM_4_DB/refined/refined_housing_data.sql
+++ models/TEAM_4_DB/refined/refined_housing_data.sql
@@ -1,2 +1,22 @@
-SELECT *
-FROM TEAM_4_DB.REFINED.HOUSING_CSV_REFINED
+WITH source_data AS (
+  SELECT *
+  FROM {{ ref('prep_housing_data') }}
+)
+
+SELECT
+  id,
+  price,
+  type,
+  sqfeet,
+  beds,
+  baths,
+  cats_allowed,
+  dogs_allowed,
+  smoking_allowed,
+  wheelchair_access,
+  electric_vehicle_charge,
+  comes_furnished,
+  parking_options,
+  UPPER(state) AS state
+FROM source_data
+WHERE LENGTH(CAST(price AS STRING)) BETWEEN 2 AND 5
--- models/TEAM_4_DB/refined/refined_qol_data.sql
+++ models/TEAM_4_DB/refined/refined_qol_data.sql
@@ -1,2 +1,9 @@
-SELECT *
-FROM TEAM_4_DB.REFINED.QOL_JSON_REFINED
+SELECT
+  state AS state,
+  Qol_TotalScore,
+  Qol_Life,
+  Qol_Affordability,
+  Qol_Economy,
+  Qol_Education_And_Health,
+  Qol_Safety
+FROM {{ ref('prep_qol_data') }}
